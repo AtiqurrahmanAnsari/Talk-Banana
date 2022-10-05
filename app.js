08c1774ae3b42c37acd14b0e6d813588a31e6bdc
AtiqurrahmanAnsari <atiqurrahman483@gmail.com>
2022-10-06 00:45:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+
+function getTranslationURL(text)
+{
+    return
+}
+
+function clickHandler() {
+ outputDiv.innerText = "asj asj asj asj " + txtInput.value;
+
+};
+
+btnTranslate.addEventListener("click", clickHandler);
+
